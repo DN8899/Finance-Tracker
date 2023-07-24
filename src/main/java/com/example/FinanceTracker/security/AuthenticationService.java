b446698346c314692beb5a89977b598a751af98e
Derrick <derricknovak889@gmail.com>
2023-07-24 14:11:07 -0400

    Added blueprint AuthService Also testing git

---
+++ src/main/java/com/example/FinanceTracker/security/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.example.FinanceTracker.security;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AuthenticationService {
+
+}
